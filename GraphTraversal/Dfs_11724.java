bd9adc117c58ec640a1817c3430e5e54aaf3387a
dain0826 <62243967+dain0826@users.noreply.github.com>
2023-10-08 00:40:30 +0900

    11724 연결요소의 개수

--- GraphTraversal/Dfs_11724.java
+++ GraphTraversal/Dfs_11724.java
@@ -1,2 +1,50 @@
-package GraphTraversal;public class Dfs_11724 {
+package GraphTraversal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Dfs_11724 {
+    static ArrayList<Integer>[] A;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        A = new ArrayList[n+1];
+        visited = new boolean[n+1];
+        for(int i=1; i<=n; i++) {
+            A[i] = new ArrayList<>();
+        }
+        for(int i=0;i<m;i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            A[a].add(b);
+            A[b].add(a);
+        }
+        int count = 0;
+        for(int i=1; i<= n; i++){
+            if(!visited[i]){
+                count++;
+                DFS(i);
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static void DFS(int i) {
+        if(visited[i])
+            return;
+        visited[i] = true;
+        for(int j : A[i]) {
+            if(!(visited[j])){
+                DFS(j);
+            }
+        }
+    }
 }
