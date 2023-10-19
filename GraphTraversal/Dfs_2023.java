99b85e8ecccb5410c55251e5d972bc2f1aa7f632
dain0826 <62243967+dain0826@users.noreply.github.com>
2023-10-19 19:59:30 +0900

    2023 신기한 소수 찾기

---
+++ GraphTraversal/Dfs_2023.java
@@ -0,0 +1,42 @@
+package GraphTraversal;
+
+import java.util.Scanner;
+
+public class Dfs_2023 {
+    static int N;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        DFS(2,1);
+        DFS(3,1);
+        DFS(5,1);
+        DFS(7,1);
+    }
+
+    static void DFS(int num, int jarisu) {
+        if(N == jarisu) {
+            if(isPrime(num)){
+                System.out.println(num);
+                return;
+            }
+        }
+        for(int i = 1;i<10; i++) {
+            if(i%2 == 0) {
+                continue;
+            }
+            if(isPrime(num * 10 + i)){
+                DFS(num*10 + i, jarisu + 1);
+            }
+        }
+        return;
+    }
+
+    static boolean isPrime(int num) {
+        for(int i=2; i<= num /2; i++) {
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
