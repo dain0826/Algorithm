cf00867f8bea83ca928cf6864e2c720686a966e1
dain0826 <62243967+dain0826@users.noreply.github.com>
2023-10-22 23:10:46 +0900

    86491 최소직사각형

--- BruteForce/Programmers_86491.java
+++ BruteForce/Programmers_86491.java
@@ -1,2 +1,18 @@
-package BruteForce;public class Programmers_86491 {
+package BruteForce;
+import java.util.*;
+
+public class Programmers_86491 {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int max_w = 0;
+        int max_h = 0;
+        for(int i=0; i<sizes.length;i++) {
+            int w = Math.max(sizes[i][0],sizes[i][1]);
+            int h = Math.min(sizes[i][0],sizes[i][1]);
+            max_w = Math.max(max_w, w);
+            max_h = Math.max(max_h, h);
+        }
+        answer = max_w * max_h;
+        return answer;
+    }
 }
