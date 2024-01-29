aca574a0e524dc083eec79bc6c8b7ee4b30a418c
dain0826 <62243967+dain0826@users.noreply.github.com>
2024-01-29 23:37:55 +0900

    9655 돌게임

--- DynamicProgramming/Dp_9655.java
+++ DynamicProgramming/Dp_9655.java
@@ -2,20 +2,20 @@ package DynamicProgramming;
 
 import java.util.Scanner;
 
-public class Dp_2193 {
+public class Dp_9655 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
-        int[] D = new int[N+1];
+        int[] D = new int[1001];
         D[1] = 1;
-        D[2] = 2;
+        D[2] = 0;
         D[3] = 1;
-        for(int i=4; i<=N; i++){
-            D[i] = Math.min(D[i-1], D[i-3]) + 1;
+        for(int i = 4; i<= N; i++){
+            if(D[i-1] == 1 && D[i-3] == 1){
+                D[i] = 0;
+            }
+            else D[i] = 1;
         }
-        if(D[N] % 2 == 0){
-            System.out.println("CY");
-        }
-        else System.out.println("SK");
+        System.out.println(D[N]==1 ? "SK" : "CY");
     }
 }
