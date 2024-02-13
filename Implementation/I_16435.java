4de7f975c54e1ba7a61ebbcf82b58272cac9fd0a
dain0826 <62243967+dain0826@users.noreply.github.com>
2024-02-13 22:49:27 +0900

    16435 스네이크버드

--- Implementation/I_16435.java
+++ Implementation/I_16435.java
@@ -1,2 +1,27 @@
-package Implementation;public class I_16435 {
+package Implementation;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class I_16435{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int L = sc.nextInt();
+        int fruit[] = new int[N];
+
+        for(int i=0; i<N; i++) {
+            fruit[i] = sc.nextInt();
+        }
+        Arrays.sort(fruit);
+        for(int i : fruit) {
+            if(L >= i) {
+                L++;
+            }
+            else
+                break;
+        }
+        System.out.println(L);
+    }
 }
