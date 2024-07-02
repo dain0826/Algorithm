0bdb4c54b4af1d084bbbc6c4babdba1bcaa4fb3c
손다인 <dain0826@naver.com>
2024-07-02 16:44:44 +0900

    2293 동전1

---
+++ DynamicProgramming/Dp_2293.java
@@ -0,0 +1,28 @@
+package DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Dp_2293 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] dp = new int[k+1]; //1원부터 k원까지
+        int[] coin = new int[n+1]; //n개의 코인
+        for(int i=1; i<=n; i++){
+            st = new StringTokenizer(br.readLine());
+            coin[i] = Integer.parseInt(st.nextToken());
+        }
+        dp[0] = 1;
+        for(int i=1; i<=n; i++){
+            for(int j= coin[i]; j<=k; j++){
+                dp[j] = dp[j] + dp[j-coin[i]]; //
+            }
+        }
+        System.out.println(dp[k]);
+    }
+}
