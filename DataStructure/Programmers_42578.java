d96c92f91c8cf2ef6eec9d1ff0d012ce1b8fd9f5
손다인 <dain0826@naver.com>
2024-11-01 00:00:49 +0900

    42578 의상

--- DataStructure/Programmers_42578.java
+++ DataStructure/Programmers_42578.java
@@ -1,2 +1,19 @@
-package DataStructure;public class Programmers_42578 {
+package DataStructure;
+
+import java.util.*;
+
+public class Programmers_42578 {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> kindOfCloth = new HashMap<>();
+        for(String[] c : clothes){
+            String type = c[1];
+            kindOfCloth.put(type, kindOfCloth.getOrDefault(type,0)+1);
+        }
+        int answer = 1;
+        for(int count : kindOfCloth.values()){
+            answer *= (count + 1);
+        }
+        return answer-1;
+    }
 }
+
